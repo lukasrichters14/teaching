2c95c451df28aba79e30b319c3d49ce85b212c4a
Lukas <lukasrichters14@gmail.com>
2019-12-20 18:07:34 -0500

    Comments added to EPLDA file

--- EPLDA.py
+++ EPLDA.py
@@ -15,23 +15,29 @@ def create_dict(file_ptr):
         {season:{team_name:points}} '''
     
     return_dict = {}
-    current_season = "18-19"
     temp_dict = {}
+    # Data file starts at the 18-19 season.
+    current_season = "18-19"
     
     # Goes through each line in the file.
     for i, line in enumerate(file_ptr):
         # i is the index, line is the line as a string.
         
+        # Skips the headers.
         if i > 0:
             # File is .csv, so split on commas.
             line_list = line.split(",")
             season = line_list[0]
             team = line_list[1]
+            # Cast points to int to be easier to evaluate.
             points = int(line_list[2])
             
+            # Keep track of data only for the current season.
             if season == current_season:
                 temp_dict[team] = points
             
+            # If we get to the next season, put the last season's  data in the
+            # return dictionary and start collecting data for this season.
             else:
                 return_dict[current_season] = temp_dict
                 current_season = season
@@ -43,6 +49,7 @@ def create_dict(file_ptr):
 
 
 def main():
+    ''' Controls the flow of the program. '''
     
     file = open_file("EPL stats.csv")
     
